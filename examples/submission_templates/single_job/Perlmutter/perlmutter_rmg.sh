c3aa766e5899f898103a4901314d716ce10cb060
Ryan Morelock <rjmorelock@login10.frontier.olcf.ornl.gov>
2025-06-02 08:54:29 -0400

    Added Perlmutter single job submission file

---
+++ examples/submission_templates/single_job/Perlmutter/perlmutter_rmg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Change to your account
+# Also change in the srun command below
+#SBATCH -A {ALLOCATION}
+#SBATCH -C gpu
+#
+# Job naming stuff
+#SBATCH -J {JOB_NAME}
+#SBATCH -o %x-%j.out
+#SBATCH -e %x-%j.err
+#
+# Requested time
+#SBATCH -t {TIME}
+#
+# Requested queue
+#SBATCH -q {PARTITION}
+
+# Number of frontier nodes to use.
+#SBATCH -N {NODES}
+#
+export SLURM_CPU_BIND="cores"
+#
+export OMP_NUM_THREADS=8
+export RMG_NUM_THREADS=9
+export OMP_WAIT_POLICY="passive"
+#
+# Load modules
+
+# Set variables
+RMG_BINARY={RMG_EXECUTABLE}
+NNODES={NODES}
+GPUS_PER_NODE={GPUS_PER_NODE}
+
+srun -A {ALLOCATION} --ntasks-per-node=$GPUS_PER_NODE -c {CORES_PER_TASK} --gpus-per-task={GPUS_PER_TASK} $RMG_BINARY {RMG_FILE_PATH}
